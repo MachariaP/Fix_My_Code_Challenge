084c1ac449e5fe3613ba65ce11075f1b30b6c776
MachariaP <walburphinehas78@gmail.com>
2024-02-25 13:54:57 +0300

    Made modifications In the while loop, the condition to check if the integer should be inserted at the current position is incorrect. It should be result[i] > i_arg instead of result[i] < i_arg.

--- 0x00-challenge/2-sort.rb
+++ 0x00-challenge/2-sort.rb
@@ -4,28 +4,26 @@
 #
 ###
 
-result = []
-ARGV.each do |arg|
-    # skip if not integer
-    next if arg !~ /^-?[0-9]+$/
+result = []  # Initialize an empty array to store sorted integers
 
-    # convert to integer
-    i_arg = arg.to_i
-    
-    # insert result at the right position
-    is_inserted = false
-    i = 0
-    l = result.size
-    while !is_inserted && i < l do
-        if result[i] < i_arg
-            i += 1
+ARGV.each do |arg|  # Iterate over each command line argument provided
+    next if arg !~ /^-?[0-9]+$/  # Skip the argument if it's not an integer
+
+    i_arg = arg.to_i  # Convert the argument to an integer
+
+    is_inserted = false  # Flag to track if the integer has been inserted
+    i = 0  # Initialize a counter variable for position tracking
+    l = result.size  # Get the size of the result array
+
+    while !is_inserted && i < l do  # Iterate over the result array until the integer is inserted
+        if result[i] > i_arg  # Check if the current element is greater than the integer
+            result.insert(i, i_arg)  # Insert the integer at the current position
+            is_inserted = true  # Set the flag to true since the integer has been inserted
         else
-            result.insert(i - 1, i_arg)
-            is_inserted = true
-            break
+            i += 1  # Move to the next position in the result array
         end
     end
-    result << i_arg if !is_inserted
+    result << i_arg if !is_inserted  # Append the integer to the result array if it hasn't been inserted yet
 end
 
-puts result
+puts result  # Print the sorted integers
